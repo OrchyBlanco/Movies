e1fa9d83bee2eba3136cff9c650468d156ae85f4
Jejormi <jejormi@gmail.com>
2023-03-31 13:15:54 +0200

    Rebuild

--- Movies/src/components/Empty.jsx
+++
@@ -1,5 +0,0 @@
-import style from "./style/Empty.module.css";
-
-export function Empty() {
-    return <div className={style.empty}>Sin Resultados</div>;
-  }
--- Movies/src/components/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import style from "./style/Spinner.module.css";
-
-export function Spinner(){
-    return(
-        <div className={style.spinner} >
-            <span className={style.loader}></span>   
-        </div>
-    );
-}
